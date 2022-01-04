71822d3876fc32fc92224c36a78538cc1f088b0b
TUGAY MANDAL <46906989+Androuinn@users.noreply.github.com>
2022-01-04 23:03:24 +0300

    fix

--- Mandalium.Core/Mandalium.Core.Abstractions/Interfaces/IGenericRepository.cs
+++ Mandalium.Core/Mandalium.Core.Abstractions/Interfaces/IGenericRepository.cs
@@ -1,10 +1,13 @@
-﻿namespace Mandalium.Core.Abstractions.Interfaces
+﻿using Mandalium.Core.Generic.Collections;
+
+namespace Mandalium.Core.Abstractions.Interfaces
 {
     public interface IGenericRepository<T> where T : class
     {
         Task<T> Get<Type>(Type id);
         Task<IEnumerable<T>> GetAll();
         Task<IEnumerable<T>> GetAll(ISpecification<T> specification);
+        Task<PagedCollection<T>> GetAllPaged(ISpecification<T> specification);
         Task Delete<Type>(Type id);
         Task Update(T entity);
         Task Save(T entity);
