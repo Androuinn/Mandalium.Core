e47021663fa03764419ea48d8d5145a3d9cf47c5
TUGAY MANDAL <46906989+Androuinn@users.noreply.github.com>
2022-01-05 23:10:59 +0300

    Update PagedCollection.cs

--- Mandalium.Core/Mandalium.Core.Generic/Generic/Collections/PagedCollection.cs
+++ Mandalium.Core/Mandalium.Core.Generic/Generic/Collections/PagedCollection.cs
@@ -4,13 +4,27 @@
     {
         public int TotalCount { get; init; }
         public ICollection<T> Collection { get; set; }
+        public int PageCount => Math.Max((int)Math.Ceiling((decimal)TotalCount / PageSize), 1);
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
 
-        public PagedCollection() {}
-
-        public PagedCollection(int totalCount, ICollection<T> collection )
+        public PagedCollection()
         {
-            this.TotalCount = totalCount;  
+            Collection ??= new List<T>();
+        }
+
+        public PagedCollection(int totalCount, ICollection<T> collection)
+        {
+            this.TotalCount = totalCount;
             this.Collection = collection;
+            this.PageSize = 10;
+        }
+
+        public PagedCollection(int totalCount, int pageSize, ICollection<T> collection)
+        {
+            this.TotalCount = totalCount;
+            this.Collection = collection;
+            this.PageSize = pageSize;
         }
     }
 }
